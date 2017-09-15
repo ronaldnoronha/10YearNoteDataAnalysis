9c9a8b359cdb1fd75cab747095c884bf330d60de
ronaldnoronha <rnor3536@uni.sydney.edu.au>
2017-09-15 13:37:00 +1000

    update on the test for front running contracts method
    Still left testing

--- ReadData/src/testCreateContractLookupTable.java
+++ ReadData/src/testCreateContractLookupTable.java
@@ -1,16 +1,17 @@
+import java.io.File;
 import java.text.*;
 import java.util.*;
 public class testCreateContractLookupTable {
 
 	public static void main(String[] args) {
-		String start = "01-10-2016";
+		String start = "01-01-2016";
 		String end = "31-12-2016";
 		CreateContractLookupTable a = new CreateContractLookupTable(start,end);
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		
 		Date tryDate = new Date();
 		try{
 			tryDate = dateFormat.parse(start);
-
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
@@ -79,18 +80,52 @@ public class testCreateContractLookupTable {
 		String[] result = {contract1,contract2};
 		return result;
 	}
-	public static int[] frontRunningContract(String[] contracts){
-		int[] result = new int[3];
+	public static int[] frontRunningContract(String[] contracts,Date date){
+		// Make a choise of which contract to pick out of the two. 
+		
 		try{
 			File fw1 = new File(contracts[0]+"_summary.txt");
+			Scanner in = new Scanner(fw1);
+			String [] line = new String[3];
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			Date summaryDate = new Date();
+			int vol1,vol2;
+			int range1,range2;
+			while (in.hasNextLine()){
+				line = in.nextLine().split(",");
+				summaryDate = dateFormat.parse(line[0]);
+				if (summaryDate.equals(date)){
+					vol1 = Integer.parseInt(line[1]);
+					range1 = Integer.parseInt(line[2]);
+					break;
+				}
+			}
+			in.close();
+			
+			// Reading the second file.
+			fw1 = new File(contracts[1]+"_summary.txt");
+			in = new Scanner(fw1);
+			while (in.hasNextLine()){
+				line = in.nextLine().split(",");
+				summaryDate = dateFormat.parse(line[0]);
+				if (summaryDate.equals(date)){
+					vol2 = Integer.parseInt(line[1]);
+					range2 = Integer.parseInt(line[2]);
+					break;
+				}
+			}
+			in.close();
+			if (vol1>vol2){
+				int[] result = {1,vol1,range1};
+			}
+			else{
+				int[] result = {2,vol2,range2};
+			}
+
 			
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
-
 		}
-
-
-
 		return result;
 	}
 
