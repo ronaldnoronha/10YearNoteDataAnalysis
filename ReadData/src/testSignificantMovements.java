504456ce571274e2c09bc737c197b79e7336fcaf
ronaldnoronha <rnor3536@uni.sydney.edu.au>
2017-10-06 14:03:25 +1100

    Created test class to read the big movements on a high vol date.

---
+++ ReadData/src/testSignificantMovements.java
@@ -0,0 +1,12 @@
+
+public class testSignificantMovements {
+
+	public static void main(String[] args) {
+		// Read the high vol dates file
+		// open the refinded movement file for each date
+		// look for max up or down above 8 ticks in between 7am and 10am
+		//
+
+	}
+
+}
