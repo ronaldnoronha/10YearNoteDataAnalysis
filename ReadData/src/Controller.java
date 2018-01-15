b1f530d98a2358ef9926565d05626ac55f20d66b
ronaldnoronha <rnor3536@uni.sydney.edu.au>
2018-01-16 01:19:24 +1100

    Testing

--- ReadData/src/Controller.java
+++ ReadData/src/Controller.java
@@ -6,7 +6,7 @@ public class Controller {
 
 	public static void main(String[] args) {
 		System.out.println(LocalTime.now());
-		String instrument = "TYAZ17";
+		String instrument = "TYAH18";
 		CreateFiles.createFiles(instrument);
 		CreateFiles.printList(instrument);
 		int numOfDays = ListOfDates.getSize();
